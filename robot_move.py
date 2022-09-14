828527f56c72d2f212f98f5048fa563ce5814cb9
Meg <megansindelar2023@u.northwestern.edu>
2022-09-14 17:18:03 -0500

    added function move user input robot joint by user input relative degree

--- robot_move.py
+++ robot_move.py
@@ -1,11 +1,16 @@
+import numpy as np
 from interbotix_xs_modules.xs_robot.arm import InterbotixManipulatorXS
 from interbotix_xs_modules.xs_robot.gripper import InterbotixGripperXSInterface
 
 robot = InterbotixManipulatorXS("px100", "arm", "gripper")
 mode = 'h'
 
+def convert_deg_to_rad(deg):
+    rad = deg*(np.pi/180)
+    return rad
+
 while mode != 'q':
-    mode = input("[h]ome, [s]leep, [q]uit, [c]lose, [o]pen, [f]orward, [b]ackward, [info]")
+    mode = input("[h]ome, [s]leep, [q]uit, [c]lose, [o]pen, [f]orward, [b]ackward, [set_single], [info]")
     if mode == "h":
         robot.arm.go_to_home_pose()
     elif mode == "s":
@@ -24,6 +29,21 @@ while mode != 'q':
         robot.arm.set_single_joint_position('shoulder', prev_shoulder - 0.1)
         prev_elbow = robot.arm.get_single_joint_command('elbow')
         robot.arm.set_single_joint_position('elbow', prev_elbow + 0.1)
+    elif mode == "set_single":
+        joint = input("Joint:  ")
+        if joint in robot.arm.group_info.joint_names:
+            deg_move = int(input("Degrees (relative):  "))
+            rad = convert_deg_to_rad(deg_move)
+            prev = robot.arm.get_single_joint_command(joint)
+            ind = robot.arm.group_info.joint_names.index(joint)
+            min = robot.arm.group_info.joint_lower_limits[ind]
+            max = robot.arm.group_info.joint_upper_limits[ind]
+            if min <= rad <= max:
+                robot.arm.set_single_joint_position(joint, prev + rad)
+            else:
+                print("Outside of limits. Min:  " + min + "  Max:  " + max)
+        else:
+            print("Not a valid joint")
     elif mode == "info":
         print(robot.arm.group_info)
 
