141a96d4ee696730fed9b806795f93fb5bf4845c
Meg <megansindelar2023@u.northwestern.edu>
2022-09-14 17:00:46 -0500

    added simple move robot forward function (working!)

--- robot_move.py
+++ robot_move.py
@@ -5,18 +5,20 @@ robot = InterbotixManipulatorXS("px100", "arm", "gripper")
 mode = 'h'
 
 while mode != 'q':
-    mode = input("[h]ome, [s]leep, [q]uit, [c]lose, [o]pen")
+    mode = input("[h]ome, [s]leep, [q]uit, [c]lose, [o]pen, [f]orward, [info]")
     if mode == "h":
         robot.arm.go_to_home_pose()
     elif mode == "s":
         robot.arm.go_to_sleep_pose()
-    # elif mode == "ee_pos":
-    #     x = int(input("x:  "))
-    #     y = int(input("y:  "))
-    #     z = int(input("z:  "))
-    #     robot.arm.set_ee_pose_components(x,y,z)
     elif mode == "c":
         robot.gripper.grasp()
     elif mode == "o":
         robot.gripper.release()
-    
+    elif mode == "f":
+        prev_shoulder = robot.arm.get_single_joint_command('shoulder')
+        robot.arm.set_single_joint_position('shoulder', prev_shoulder + 0.1)
+        prev_elbow = robot.arm.get_single_joint_command('elbow')
+        robot.arm.set_single_joint_position('elbow', prev_elbow - 0.1)
+    elif mode == "info":
+        print(robot.arm.group_info)
+
