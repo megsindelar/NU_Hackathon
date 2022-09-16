aec2c95097038cbd6aeb9ce508954280dd9c7547
Meg <megansindelar2023@u.northwestern.edu>
2022-09-16 13:38:03 -0500

    transformations are working! now moving robot to pen (waist moves correctly)

--- robot_move.py
+++ robot_move.py
@@ -11,31 +11,29 @@ def convert_deg_to_rad(deg):
     rad = deg*(np.pi/180)
     return rad
 
-x_c = realsense.coord[0]
-y_c = realsense.coord[1]
-z_c = realsense.coord[2]
-
 """Convert coordinate frame from camera to base of robot to end effector"""
 def convert_coord_frame(x_c,y_c,z_c):
     cam = [x_c, y_c, z_c, 1]
     rot_mat_z = [[np.cos(np.deg2rad(90)), -np.sin(np.deg2rad(90)), 0], [np.sin(np.deg2rad(90)), np.cos(np.deg2rad(90)), 0], [0,0,1]]
     rot_mat_x = [[1,0,0],[0, np.cos(0.2), np.sin(0.2)], [0, -np.sin(0.2), np.cos(0.2)]]
     rot_mat = np.matmul(rot_mat_x, rot_mat_z)
-    disp_vec = [264.15, 264.15, 0]
-    extra_row = [0,0,0,1]
+    disp_vec = [[0.26415], [0.26415], [0]]
+    extra_row = [[0,0,0,1]]
     homogen_rc = np.concatenate((rot_mat, disp_vec), axis=1)            #put side by side
     homogen_rc = np.concatenate((homogen_rc, extra_row), axis=0)
     rob_base = np.matmul(homogen_rc, cam)
+    print(rob_base)
     joints = robot.arm.get_joint_commands()
     T = mr.FKinSpace(robot.arm.robot_des.M, robot.arm.robot_des.Slist, joints)
     [R,p] = mr.TransToRp(T)
+    p = [[p[0]], [p[1]], [p[2]]]
     homogen_er = np.concatenate((R,p),axis=1)
     homogen_er = np.concatenate((homogen_er, extra_row), axis=0)
     ee = np.matmul(homogen_er, rob_base)
     return ee
 
 while mode != 'q':
-    mode = input("[h]ome, [s]leep, [q]uit, [c]lose, [o]pen, [f]orward, [b]ackward, [set_single], [info]")
+    mode = input("[h]ome, [s]leep, [q]uit, [c]lose, [o]pen, [f]orward, [b]ackward, [set_single], [pen], [info]")
     if mode == "h":
         robot.arm.go_to_home_pose()
         elbow = robot.arm.get_single_joint_command('elbow')
@@ -43,7 +41,9 @@ while mode != 'q':
         prev_elbow = robot.arm.get_single_joint_command('elbow')
         robot.arm.set_single_joint_position('elbow', prev_elbow + 0.2)
         elbow = robot.arm.get_single_joint_command('elbow')
-        print(elbow)
+        # prev_waist = robot.arm.get_single_joint_command('waist')
+        # robot.arm.set_single_joint_position('waist', prev_waist + 1.57)
+        # print(elbow)
     elif mode == "s":
         robot.arm.go_to_sleep_pose()
     elif mode == "c":
@@ -75,6 +75,27 @@ while mode != 'q':
                 print("Outside of limits. Min:  " + min + "  Max:  " + max)
         else:
             print("Not a valid joint")
+    elif mode == "pen":
+        """go to home"""
+        robot.arm.go_to_home_pose()
+        elbow = robot.arm.get_single_joint_command('elbow')
+        print(elbow)
+        prev_elbow = robot.arm.get_single_joint_command('elbow')
+        robot.arm.set_single_joint_position('elbow', prev_elbow + 0.2)
+        elbow = robot.arm.get_single_joint_command('elbow')
+
+        """realsense"""
+        realsense.sense_pen()
+        print(realsense.coord)
+        x_cam = realsense.coord[0]
+        y_cam = realsense.coord[1]
+        z_cam = realsense.coord[2]
+        end_eff = convert_coord_frame(x_cam, y_cam, z_cam)  
+        waist = -np.cos(end_eff[0]/end_eff[1])
+        print(f"waist: {waist}")
+        robot.arm.set_single_joint_position('waist', waist)
+        print(end_eff)
+        
     elif mode == "info":
         print(robot.arm.group_info)
 
